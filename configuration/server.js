c692004292b3065916df7025dc74f05d3acfd055
nicolas <nicolas.esdero00@gmail.com>
2021-09-01 17:53:32 -0300

    editing port connection

--- configuration/server.js
+++ configuration/server.js
@@ -8,6 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({extend: true}));
 app.use(morgan("dev"));
 //routes
+app.get('/',(req,res)=>{
+    res.send('Welcome to Api Songs');
+});
 app.use('/song',require('../routes/song'));
 
 module.exports=app;
